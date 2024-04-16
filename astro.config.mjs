7bb70f066ce0e9e43d16f70046acf4b099f028f7
Eduardo De Matos <nanudmr@gmail.com>
2024-04-16 16:53:34 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'nakecode.com',
-    base: '/'
+    site: 'https://nakecode.com',
+
 });
